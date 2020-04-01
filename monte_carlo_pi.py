2233c6854ef34cb1f6a868f088b92dc15c71ea3b
Marcos H <zukr4m@gmail.com>
2020-03-31 21:45:43 -0300

    Small fixes

--- monte_carlo_pi.py
+++ monte_carlo_pi.py
@@ -5,18 +5,21 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+pi = 0
 
 if rank == 0:
     for i in range(1, size):
         pi += comm.recv(source=i, tag=i)
-    pi = pi / size
+    pi = pi / (size-1)
     print(pi)
 else:
-    for count in range(0, 1000):
+    count = 0
+    count_inside = 0
+    for count in range(0, 100000):
         d = math.hypot(random.random(), random.random())
         if d < 1:
             count_inside += 1
-    count += 1
+        count += 1
     pi = (4.0 * count_inside / count)
     comm.send(pi, dest=0, tag=rank)
 
